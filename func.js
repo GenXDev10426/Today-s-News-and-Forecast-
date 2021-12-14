b7b66c6833f7f5534c33b123b4eb24a640bcd1ca
Pravin kumar Ravi <btech10426.19@bitmesra.ac.in>
2021-12-14 17:17:03 +0530

    updated

--- func.js
+++ func.js
@@ -1,6 +1,33 @@
-/* Api Setup */ 
+/* Api configuration */ 
+const apiConfig = {
+    countryUrl : " https://api.countrystatecity.in/v1/countries",
+    countryKey : " ",
+    WeatherUrl : "https://api.openweathermap.org/data/2.5/",
+    weatherKey : "aa545e53cce6c7640429fe26b05cb90b",
+}               
 
- 
+// selcting countries from drop down 
+
+const getCountries = async() => {
+    let apiEndPoint = apiConfig.countryUrl;
+    const response = await fetch(apiEndPoint, {headers:{"x-CSCAPI-KEY" : apiConfig.countryKey},
+});
+    if(response.status!=200){
+        throw new Error(`Something went wrong ,status code: ${response.status}`);
+    }
+    const countries =await response.json();
+    return countries;
+};
+
+document.addEventListener("DOMContentLoaded", async() => {
+    const countries=await getCountries();
+})
 
 
 
+/* search inputs on key press*/
+const searchInputBox = document.getElementById('input-box');
+searchInputBox.addEventListener('keypress' , (event) => {
+    
+    console.log(searchInputBox.value);
+})
